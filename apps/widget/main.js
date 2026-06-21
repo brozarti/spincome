06f76cd1694b020bd8b6a86a9d33ac805e8c1208
brozarti <zoneb17@gmail.com>
2026-06-20 22:58:35 -0400

    Increase widget height to fit ad card without clipping bottom

--- apps/widget/main.js
+++ apps/widget/main.js
@@ -41,7 +41,7 @@ function createWindow() {
   const trayBounds = tray.getBounds();
   const display = screen.getPrimaryDisplay();
   const winW = 320;
-  const winH = 400;
+  const winH = 560;
 
   const x = Math.round(trayBounds.x + trayBounds.width / 2 - winW / 2);
   const y = trayBounds.y + trayBounds.height + 4;
